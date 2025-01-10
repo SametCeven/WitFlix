6047799aa38feff61e806006f1dbb9f8c0976c1d
SametCeven <49654402+SametCeven@users.noreply.github.com>
2025-01-10 14:58:23 +0300

    added api example

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ const xdom = document.querySelector(".btn-lg");
 xdom.addEventListener("click",(e)=>{
     console.log(e);
 })
+
+const x = axios.get('https://apis.ergineer.com/ipadresim')
+
+
+async function myfunc(){
+    let y;
+    await axios
+        .get('https://apis.ergineer.com/ipadresim')
+        .then((response)=>{
+            y = response
+        })
+        .catch((error)=>{
+            y = error
+        })
+    return y;
+}
+
+myfunc().then((reponse)=>{
+    console.log(reponse)
+})
+
