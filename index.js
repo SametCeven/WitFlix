57083526457bc3894424d7e5b7bdc2e67becc65e
SametCeven <49654402+SametCeven@users.noreply.github.com>
2025-01-13 22:17:29 +0300

    added movie details via api

--- index.js
+++ index.js
@@ -4,23 +4,45 @@ xdom.addEventListener("click",(e)=>{
     console.log(e);
 })
 
-const x = axios.get('https://apis.ergineer.com/ipadresim')
+// omdb api key
+// https://www.omdbapi.com/
+const apiKey = "db117d29";
 
-
-async function myfunc(){
-    let y;
+async function getMovieData(searchTerm,apiKeyT=apiKey){
+    let x;
     await axios
-        .get('https://apis.ergineer.com/ipadresim')
+        .get(`https://www.omdbapi.com/?t=${searchTerm}&apikey=${apiKeyT}`)
         .then((response)=>{
-            y = response
+            x = response.data
         })
         .catch((error)=>{
-            y = error
+            x = error
         })
-    return y;
+    return x;
 }
 
-myfunc().then((reponse)=>{
-    console.log(reponse)
-})
+const movieSearchList = ["The Matrix","Dune","Armageddon","The Prestige","Inception","Tenet","Marvel","İron Man","Black Panther"]
+const suggestionImgArr = document.querySelectorAll(".suggestion-img");
+const suggestionTitleArr = document.querySelectorAll(".suggestion-item-content h3");
+const suggestionDurationArr = document.querySelectorAll(".duration"); 
+const suggestionRatedArr = document.querySelectorAll(".maturity-rating"); 
+let movieSearchListData = [];
+
+async function addSectionMovieData(obj) {
+    for(let i=0; i<movieSearchList.length;i++){
+        await getMovieData(movieSearchList[i]).then((response)=>{
+            movieSearchListData.push(response);
+        })
+        console.log(movieSearchListData[i]);
+        suggestionImgArr[i].src = movieSearchListData[i].Poster;
+        suggestionTitleArr[i].textContent = movieSearchListData[i].Title;
+        suggestionDurationArr[i].textContent = movieSearchListData[i].Runtime;
+        suggestionRatedArr[i].textContent = movieSearchListData[i].Rated;
+    }
+}
+
+addSectionMovieData()
+
+
+
 
